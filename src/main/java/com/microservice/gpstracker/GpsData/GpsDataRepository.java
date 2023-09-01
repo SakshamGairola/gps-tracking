8da19f50907070104498998d4846d9e2e6bf2547
Saksham <saksham.gairola28@gmail.com>
2023-09-01 16:19:15 +0530

    declared method to get record between two dates `findAllGPSDataByVehicleModelVehicleIdAndLocationDateBetween`

--- src/main/java/com/microservice/gpstracker/GpsData/GpsDataRepository.java
+++ src/main/java/com/microservice/gpstracker/GpsData/GpsDataRepository.java
@@ -3,11 +3,15 @@ package com.microservice.gpstracker.GpsData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
 import java.util.List;
 
 @Repository
 public interface GpsDataRepository extends JpaRepository<GpsDataModel, Long> {
 
     List<GpsDataModel> findAllGpsDataByVehicleModelVehicleId(Long vehicleId);
+
+    List<GpsDataModel> findAllGPSDataByVehicleModelVehicleIdAndLocationDateBetween(Long vehicleId, Date startDate,
+                                                                           Date endDate);
 
 }
