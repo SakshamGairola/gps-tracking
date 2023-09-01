60132459d4d5eb4b05937caf10f9612a537147b0
Saksham <saksham.gairola28@gmail.com>
2023-09-01 16:16:21 +0530

    add method to calculate distance, re-implemented API's to account for unauthorized access

--- src/main/java/com/microservice/gpstracker/GpsData/GpsDataController.java
+++ src/main/java/com/microservice/gpstracker/GpsData/GpsDataController.java
@@ -1,13 +1,18 @@
 package com.microservice.gpstracker.GpsData;
 
 import com.microservice.gpstracker.Vehicle.VehicleService;
+import com.microservice.gpstracker.util.AuthorizeUtility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.util.List;
 
+@SuppressWarnings("ALL")
 @RestController
-@RequestMapping("/gpsData/{owner_id}/{vehicle_id}")
+@RequestMapping("/gpsData/{owner_id}")
 public class GpsDataController {
 
     @Autowired
@@ -16,22 +21,68 @@ public class GpsDataController {
     @Autowired
     private VehicleService vehicleService;
 
-    @GetMapping("/getInfo")
-    public List<GpsDataModel> findAllGpsDataByVehicleModelVehicleId(Long vehicleId) {
-        List<GpsDataModel> test = this.gpsDataService.findAllGpsDataByVehicleModelVehicleId(1L);
-        System.out.println(test);
-        return test;
+    @Autowired
+    private AuthorizeUtility utility;
+
+    @GetMapping("/getInfo/{vehicle_id}")
+    public ResponseEntity<Object> findAllGpsDataByVehicleModelVehicleId(@PathVariable("owner_id") Long owner_id,
+                                                                        @PathVariable("vehicle_id") Long vehicle_id) {
+        if (utility.checkIfValid(owner_id, vehicle_id)) {
+            List<GpsDataModel> vehicleGpsData = this.gpsDataService.findAllGpsDataByVehicleModelVehicleId(vehicle_id);
+            return new ResponseEntity<>(vehicleGpsData, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Unauthorised", HttpStatus.UNAUTHORIZED);
     }
 
-    @PostMapping("/save")
-    public String saveVehicleCoordinates(@PathVariable("vehicle_id") Long vehicle_id,
-                                         @RequestBody GpsDataModel gpsDataModel) {
-        this.vehicleService.getVehicleById(vehicle_id).map(vehicle -> {
+    @PostMapping("/save/{vehicle_id}")
+    public GpsDataModel saveVehicleCoordinates(@PathVariable("vehicle_id") Long vehicle_id,
+                                               @RequestBody GpsDataModel gpsDataModel) {
+        System.out.println(gpsDataModel);
+        return this.vehicleService.getVehicleById(vehicle_id).map(vehicle -> {
             gpsDataModel.setVehicleModel(vehicle);
             return gpsDataService.saveGpsDataModel(gpsDataModel);
         }).get();
 
-        return null;
+    }
+
+    @GetMapping("/getDist/{vehicle_id}")
+    public ResponseEntity<Object> getDistanceTravelled(@PathVariable("owner_id") Long owner_id, @PathVariable(
+            "vehicle_id") Long vehicle_id, @RequestParam("startDate") String startDate,
+                                                       @RequestParam("endDate") String endDate) {
+        if (utility.checkIfValid(owner_id, vehicle_id)) {
+            Double distance = calculateDistance(this.gpsDataService.getAllCoordinates(vehicle_id,
+                    Date.valueOf(startDate), Date.valueOf(endDate)));
+            return new ResponseEntity<>(distance, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Unauthorised", HttpStatus.UNAUTHORIZED);
+    }
+
+    private Double calculateDistance(List<GpsDataModel> coordinateList) {
+        if (coordinateList.size() < 2) return 0D;
+
+        double totalDistance = 0D;
+
+        double earthRadiusKM = 6371;
+
+        for (int i = 0; i < coordinateList.size() - 1; i++) {
+
+            double longitude1 = Math.toRadians(coordinateList.get(i).getLongitude()), latitude1 =
+                    Math.toRadians(coordinateList.get(i).getLatitude());
+
+            double longitude2 = Math.toRadians(coordinateList.get(i + 1).getLongitude()), latitude2 =
+                    Math.toRadians(coordinateList.get(i + 1).getLatitude());
+
+            double diffLongitude = (longitude2 - longitude1), diffLatitude = (latitude2 - latitude1);
+
+            double a =
+                    Math.pow(Math.sin(diffLatitude / 2), 2) + Math.cos(latitude1) * Math.cos(latitude2) * Math.pow(Math.sin(diffLongitude / 2), 2);
+
+            double c = 2 * Math.asin(Math.sqrt(a));
+
+            totalDistance += c * earthRadiusKM;
+        }
+
+        return totalDistance;
     }
 
 }
